cef69ffb1e78a08d9f44a2b301000674cb42ba01
zAeryos <dave_13_08@hotmail.it>
2024-03-18 22:33:29 +0100

    Homepage created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [
+  {
+    path        : '',
+    redirectTo  : 'homepage',
+    pathMatch   : 'full'
+  },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'homepage', loadChildren: () => import('./pages/homepage/homepage.module').then(m => m.HomepageModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
