5048ad4c379f09d2e7a72b4a336f7d7605ff0656
zAeryos <dave_13_08@hotmail.it>
2024-03-19 02:46:19 +0100

    Created simple form for register and login, set basic routing

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: AuthComponent },
-
+  {
+    path:       'register',
+    component:  RegisterComponent
+  },
+  {
+    path:       'login',
+    component:  LoginComponent
+  }
 ];
 
 @NgModule({
